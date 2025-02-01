c7597fd79282876665ff672ec24482e875fe33ef
Cristina Chico <cristina.chidel@gmail.com>
2025-02-01 17:29:37 +0000

    avance proyecto hasta var num-num

--- src/app.py
+++ src/app.py
@@ -1,4 +1,24 @@
 from utils import db_connect
 engine = db_connect()
 
+
 # your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data.head()
+
+total_data.shape
+
+total_data.info()
+
+total_data.drop("id", axis = 1).duplicated().sum()
+
+total_data = total_data.drop_duplicates(subset = total_data.columns.difference(['id']))
+print(total_data.shape)
+total_data.head()
+
+if total_data.duplicated().sum():
+    total_data = total_data.drop_duplicates()
+print(total_data.shape)
+total_data.head()
